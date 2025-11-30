c21f58b1fb15ad0a6f2dde59aa2f7b02283a2de3
Hamid-Usman <abdulhamidusman218gmail.com>
2025-11-30 04:38:35 -0800

    - reduced cohort creation to only one - handled initial page routing to check on previous userData

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'expo-router';
 import { supabase } from '@/utils/lib/supabase';
 import { getItem } from '../utils/asyncStorage';
 import { View, ActivityIndicator } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,11 +17,19 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 export default function InitialScreen() {
   const router = useRouter();
 
   useEffect(() => {
     const initialize = async () => {
+      const userDataString = await AsyncStorage.getItem('userData');
+      console.log(userDataString); // This will show the string: "{\"id\":24,\"email\":\"u@a.com\",\"role\":\"convener\"}"
+      
+      // Parse the string to get the user object
+      const user = userDataString ? JSON.parse(userDataString) : null;
+      console.log(user); // This will show the parsed object
+      
       const onboarded = await getItem('onboarded');
       const {
         data: { session },
@@ -29,10 +38,16 @@ export default function InitialScreen() {
 
       if (!onboarded) {
         router.replace('/(auth)/onboarding');
-        // } else if (session && session.user) {
-        // router.replace('/(home)/home');
-      } else {
-        router.replace('/(auth)/login');
+        return; // Early return to avoid further checks
+      }
+      else if (user?.role === 'convener') {
+        router.replace('/convener-screens/(cohorts)'); 
+      }
+      else if (user?.role === 'learner') {
+        router.replace('/student-screens/cohorts'); 
+      }
+      else {
+        router.replace('/(auth)/auth');
       }
     };
 
