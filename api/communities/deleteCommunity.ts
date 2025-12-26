4767205689f05e01be70de782c45f298a8710849
Hamid-Usman <abdulhamidusman218gmail.com>
2025-12-26 16:42:05 +0100

    changed alert text

--- api/communities/deleteCommunity.ts
+++ api/communities/deleteCommunity.ts
@@ -68,7 +68,8 @@ export const useRemoveCommunity = () => {
                 queryKey: ['community', variables.id],
             });
 
-            Alert.alert('Success', 'Learner removed successfully');
+
+            Alert.alert('Success', 'Community removed successfully');
         },
 
         onError: (error) => {
