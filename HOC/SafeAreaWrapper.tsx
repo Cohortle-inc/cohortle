b97447d9d612ab8a015a911b444c02a560e1d058
Hamid-Usman <abdulhamidusman218gmail.com>
2025-12-26 08:43:57 +0100

    changed version

--- HOC/SafeAreaWrapper.tsx
+++ HOC/SafeAreaWrapper.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 import {
   SafeAreaView,
-  SafeAreaView,
   // StyleProp,
   // ViewStyle,
 } from 'react-native-safe-area-context';
